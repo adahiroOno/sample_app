304d226fb109884db4a57ed69376732decbead8a
sadahiro <sadahiro0310@gmail.com>
2018-08-09 21:44:35 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '064d00c3e06933bdbdbf827bdd2b1441d3e83597d886cc656a90cd3f8ec7f9de2002f18c5654799ea769eec0ea20574edf95d4eeda21747ebbaab5e468073968'
---
+++ vendor/bundler/gems/ffi-1.9.25/ext/ffi_c/libffi-x86_64-darwin17.7.0/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/sadahiro/rails_projects/sample_app/vendor/bundler/gems/ffi-1.9.25/ext/ffi_c/libffi/src/x86/ffitarget.h
