283e1db468de7ac295df065102aceaa0ecfc2934
sadahiro <sadahiro0310@gmail.com>
2018-09-19 15:19:44 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
-  
+def feed
+  microposts
+end
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,3 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
 include ApplicationHelper
 
 def valid_signin(user)
@@ -17,4 +10,12 @@ RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
   end
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Capybaraを使用していない場合にもサインインする。
+  cookies[:remember_token] = user.remember_token
+end
 end
